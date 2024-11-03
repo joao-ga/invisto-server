f47de03e8c09e02950ad101e67ea335465211fc7
Caio Acosta <caioacosta4@gmail.com>
2024-11-03 12:20:29 -0300

    Creating threads for connections

--- src/ServerConnection.java
+++ src/ServerConnection.java
@@ -17,8 +17,22 @@ public class ServerConnection {
                 System.out.println("Conexão aceita: " + clientSocket.getInetAddress());
 
                 // Delegar o processamento do cliente para o ClientRequestProcessor
-                ClientRequestProcessor processor = new ClientRequestProcessor(clientSocket);
-                processor.processRequest();
+                // Criando uma thread p/ cada conexão, para possibilitar conexões paralelas
+                new Thread(() -> {
+                    try {
+                        ClientRequestProcessor processor = new ClientRequestProcessor(clientSocket);
+                        processor.processRequest();
+                        System.out.println("Thread criada com sucesso!");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            clientSocket.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
             }
         } catch (Exception e) {
             e.printStackTrace();
